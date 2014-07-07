f3092d5159dd28a43a6e3e8166a32681efadb497
Wye Yu <k.yu@unsw.edu.au>
2014-07-07 11:14:50 +1000

    update unsw iconbar images and css

--- unswiconbar/unswiconbar-iconbar-links.tpl.php
+++ unswiconbar/unswiconbar-iconbar-links.tpl.php
@@ -1,6 +1,6 @@
 <ul id="icons">
   <li class="share">
-    <a href="#" class="icon share" title="Share">Share</a>
+    <a href="" class="icon share" title="Share">Share</a>
     <div class="share-box">
       <h4>Share this page via social media:</h4>
       <ul class="share-links">
@@ -9,9 +9,10 @@
         <li><a href="http://www.newsvine.com/_wine/save" class="newsvine" onclick="window.open('http://www.newsvine.com/_wine/save?u='+encodeURIComponent('<?php print $page_url; ?>'),'Newsvine', 'toolbar=no,width=550,height=550'); return false;">Newsvine</a></li>
         <li><a href="http://www.google.com/bookmarks/mark" class="googlebookmarks" onclick="window.open('http://www.google.com/bookmarks/mark?op=add&amp;bkmk='+encodeURIComponent('<?php print $page_url; ?>')+'&amp;title='+encodeURIComponent('<?php print $page_title; ?>'),'Google bookmarks', 'toolbar=no,width=550,height=550'); return false;">Google bookmarks</a></li>
       </ul>
-      <p><a href="mailto:email@address.here?subject=Page suggestion on Engineering@UNSW&amp;body=<?php print $page_title; ?>%0AURL: <?php print $page_url; ?>">Send an email link to this page</a></p>
+      <p><a href="mailto:gettingstarted@unsw.edu.au?subject=Page suggestion on UNSW Getting Started&amp;body=<?php print $page_title; ?>%0AURL: <?php print $page_url; ?>">Send an email link to this page</a></p>
     </div>
   </li>
   <li class="print"><a href="#" class="icon print" title="Print">Print</a></li>
 </ul>
+
 <div class="clear"></div>
